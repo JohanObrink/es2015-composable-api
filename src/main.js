031231b265b92c595643298881b6b00db850c32c
Johan Öbrink <johan.ext.obrink@swedbank.se>
2015-12-08 16:06:24 +0100

    Removed main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Api from './api';
-import auth from './api.auth';
-import search from './api.search';
-
-export default function (http) {
-  return new Api(http)
-    .mixin(auth)
-    .mixin(search);
-}
